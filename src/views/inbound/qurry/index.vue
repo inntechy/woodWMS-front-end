284404ee3209133c38a04254ce755867e5395774
inntechy <yinpassing@qq.com>
2018-08-10 10:56:28 +0800

    基本完成入库单表格页面

--- src/views/inbound/qurry/index.vue
+++ src/views/inbound/qurry/index.vue
@@ -1,14 +1,47 @@
 <template>
   <div class="app-container">
-    <el-table :data="list" v-loading="listLoading" element-loading-text="Loading" border fit highlight-current-row>
-      <el-table-column align="center" label='ID' width="150">
+    <el-pagination
+      v-loading="listLoading"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="list.length">
+    </el-pagination>
+    <el-table :data="list.slice((currentPage-1)*pagesize,currentPage*pagesize)" v-loading="listLoading" element-loading-text="Loading" border fit highlight-current-row>
+      <el-table-column align="center" label='ID' width="80">
         <template slot-scope="scope">
-          {{scope.$index}}
+          {{Number(scope.$index)+1}}
         </template>
+      </el-table-column>
+       <el-table-column align="center" prop="createAt" label="创建时间" width="200">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span>{{scope.row.createAt}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label='货号' width="100" prop="goods_mark">
       </el-table-column>
       <el-table-column label="品牌/品名">
         <template slot-scope="scope">
           {{scope.row.brand}}/{{scope.row.name}}
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label='总支数' width="100" prop="quanlity" />
+      <el-table-column align="center" label='总体积' width="100" prop="volume_sum">
+        <template slot-scope="scope">
+          {{scope.row.volume_sum + ' m³'}}
+        </template>
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClickLook(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -21,8 +54,10 @@ import { getList } from '@/api/inbound'
 export default {
   data() {
     return {
-      list: null,
-      listLoading: true
+      list: { test: 'test' },
+      listLoading: true,
+      currentPage: 1,
+      pagesize: 10
     }
   },
   filters: {
@@ -42,11 +77,20 @@ export default {
     fetchData() {
       this.listLoading = true
       getList().then(response => {
-        this.list = response
+        this.list = response.reverse()
         this.listLoading = false
-        console.log(response)
         // 返回的数据没有变量名 所以只能直接用response
       })
+    },
+    handleClickLook(row) {
+      // 此处需要跳转到详情页面
+    },
+    // 两个处理分页的函数
+    handleSizeChange: function(size) {
+      this.pagesize = size
+    },
+    handleCurrentChange: function(currentPage) {
+      this.currentPage = currentPage
     }
   }
 }
