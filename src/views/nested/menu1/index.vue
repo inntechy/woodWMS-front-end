52e27a7c31b26754ca4c80f5b67d5b601cd78473
inntechy <yinpassing@qq.com>
2018-08-12 09:13:28 +0800

    删除无用页面

--- src/views/nested/menu1/index.vue
+++
@@ -1,7 +0,0 @@
-<template >
-  <div style="padding:30px;">
-    <el-alert title="入库单" :closable="false">
-      <router-view />
-    </el-alert>
-  </div>
-</template>
--- src/views/nested/menu1/menu1-1/index.vue
+++
@@ -1,7 +0,0 @@
-<template >
-  <div style="padding:30px;">
-    <el-alert title="menu 1-1" type="faild" :closable="true">
-      <router-view />
-    </el-alert>
-  </div>
-</template>
